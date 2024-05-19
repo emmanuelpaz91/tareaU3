33199bcc7074e7e965edbcb4ae15933950be0aa8
Emmanuel Paz <emmanuelpaz.91@gmail.com>
2024-05-19 16:39:23 -0300

    Sitio con formulario funcionando

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.post('/', async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const nombre = req.body.nombre; 
+    
+
+    var transport = nodemailer.createTransport({
+      host: process.env.smtp_host,
+      port: process.env.smtp_port,
+      auth: {
+        user: process.env.smtp_user,
+        pass: process.env.smtp_pass
+      }
+    })    
+
+ // Definir el contenido del correo
+ const mailOptions = {
+  from: 'emmanuelpaz.91@gmail.com', // Cambia esto por tu dirección de correo
+  to: 'emmanuelpaz.91@gmail.com', // Cambia esto por la dirección de correo de destino
+  subject: 'Nuevo suscriptor',
+  text: `Se enviará news a ${email} con nombre ${nombre}.`
+};
+
+// Enviar el correo
+await transport.sendMail(mailOptions);
+
+res.send(`
+<script>
+  alert('Datos recibidos y correo enviado correctamente');
+  window.location.href = '/';
+</script>
+`);
+} catch (error) {
+next(error);
+}
+});
+
+module.exports = router;
